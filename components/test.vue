3849fc5510db2e4fc58899cd13a706739e00341a
kwok <kwok_fei@yeah.net>
2019-12-30 22:23:36 +0800

    弹框

---
+++ components/test.vue
@@ -0,0 +1,30 @@
+<template>
+    
+
+        <div class="popoutBox">
+            
+            <div class="popoutTitle">
+                <span>测试</span>
+                <img src="../../BCCA-Demo/img/close.png" alt="">
+            </div>
+
+        </div>
+
+
+</template>
+ 
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    }
+}
+</script>
+ 
+<style>
+.hello {
+    
+}
+</style>
