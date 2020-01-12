5fb3a700ef06c690267a4387a6ad13ecd4044e02
kwok <kwok_fei@yeah.net>
2020-01-12 13:25:55 +0800

    样式修改 跳转修改

--- components/setUI.vue
+++ components/setUI.vue
@@ -1,55 +1,55 @@
 <template>
- <!-- <iframe
+  <!-- <iframe
         :src='"http://118.31.76.126:20001/device/UIEditor?productCode="+productcode'
         frameborder="0"
         @click="close"
-      ></iframe> -->
+  ></iframe>-->
 
-      <!-- 备用 -->
+  <!-- 备用 -->
   <div class="setUIBox">
     <div class="popoutTitle setUITitle">
       <span>UI模板自动生成</span>
-          <img src="../../BCCA-Demo/img/close.png" @click="close" />
+      <img src="../../BCCA-Demo/img/close.png" @click="close" />
     </div>
     <div class="UIBox">
-
-        <div>
-          <img src="../../BCCA-Demo/img/fail.png" alt="">
-        </div>
-        <div>
-          <img src="../../BCCA-Demo/img/fail.png" alt="">
-        </div>
-        <div>
-          <img src="../../BCCA-Demo/img/fail.png" alt="">
-        </div>
-
-
-
+      <div 
+      v-for="(item,key,i) in uIList" 
+      :key = "i"
+      :class="{'activeStyle': active==item.id}" 
+      @click="changeActiveStyle(item.id)">
+        <img :src="item.src" alt />
+      </div>
     </div>
     <div class="popoutBtns setUIBtns">
-<!--        <input type="button" value="接入" @click="close1" v-show="choose_device.isSuccess && flag == 'img'" />-->
-        <input type="button" value="确定" @click="close"/>
-        <input type="button" value="重置"/>
-      </div>
+      <!--        <input type="button" value="接入" @click="close1" v-show="choose_device.isSuccess && flag == 'img'" />-->
+      <input type="button" value="确定" @click="close" />
+      <input type="button" value="重置" @click="active=''" />
+    </div>
   </div>
 </template>
  
 <script>
-
 module.exports = {
-
-
   mounted() {
     //   console.log('productcode:', this.productcode)
   },
   data: function() {
     return {
+      uIList: [
+        { id: 1, src: "../../BCCA-Demo/img/fail.png" },
+        { id: 2, src: "../../BCCA-Demo/img/fail.png" },
+        { id: 3, src: "../../BCCA-Demo/img/fail.png" }
+      ],
+      active : "", //被点击选中的UI模板
     };
   },
   props: ["productcode"],
   methods: {
     close() {
       this.$emit("close");
+    },
+    changeActiveStyle(id){
+        this.active = id;
     }
   }
 };
@@ -59,7 +59,7 @@ module.exports = {
 .setUIBox {
   width: 90%;
   margin: 0 auto;
-  background: #1B2633;
+  background: #1b2633;
 }
 .setUITitle,
 iframe .hello {
@@ -70,24 +70,31 @@ iframe .hello {
   height: 100%;
 }
 .setUIBox > div:nth-child(2) {
-  background: #131B2D;
+  background: #131b2d;
   width: 96%;
   height: calc(100vh - 2.6rem);
   margin: 0 2%;
 }
-.setUIBtns{
+.setUIBtns {
   margin-top: 0;
   background: #e2e6ec;
   text-align: center;
   padding: 0.2rem 0;
-  background: #1B2633;
+  background: #1b2633;
 }
 
 /* 模板内容盒子 */
-.UIBox{
+.UIBox {
   display: flex;
   justify-content: space-around;
   align-items: center;
-
+}
+.UIBox > div {
+  cursor: pointer;
+  border: 1px solid #1b2633;
+  padding: 0.3rem;
+}
+.UIBox .activeStyle{
+  border : 3px solid #0492F2
 }
 </style>
